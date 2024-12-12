77d3ef46ed9c5935bb8c5b54240fc034f5e63863
Anubhav Jain <jainanubhav2003@gmail.com>
2024-12-12 20:58:09 +0530

    model work

--- model/model.py
+++ model/model.py
@@ -9,16 +9,25 @@ def zcr(data, fl, hl):
 def rms(data, fl, hl):
     return np.squeeze(librosa.feature.rms(y=data, frame_length=fl, hop_length=hl))
 
-def mfcc(data, sr): return np.ravel(librosa.feature.mfcc(y=data, sr=sr).T)
+def mfcc(data, sr): 
+    return np.ravel(librosa.feature.mfcc(y=data, sr=sr).T)
 
 def extract_features(data, sr=22050, fl=2048, hl=512):
-    return np.hstack((np.array([]), zcr(data,fl,hl), rms(data, fl, hl), mfcc(data, sr)))
+    features = np.hstack((np.array([]), zcr(data, fl, hl), rms(data, fl, hl), mfcc(data, sr)))
+    target_size = 2376
+    
+    if features.shape[0] < target_size:
+        features = np.pad(features, (0, target_size - features.shape[0]), 'constant')
+    elif features.shape[0] > target_size:
+        features = features[:target_size]
 
-def get_features(audioData):
-    data, sr = librosa.load(audioData); res = np.array(extract_features(data, sr))
+    return features
+
+def get_features(path):
+    data, sr = librosa.load(path); res = np.array(extract_features(data, sr))
     return np.expand_dims(scaler.transform(np.reshape(res, newshape=(1, res.shape[0]))), axis=2)
 
-def prediction(audioData): return encoder.inverse_transform(model.predict(get_features(audioData)))
+def prediction(path): return encoder.inverse_transform(model.predict(get_features(path)))
 
 with open('./model/helper/scaler.pickle', 'rb') as f: scaler = pickle.load(f)
 with open('./model/helper/encoder.pickle', 'rb') as f: encoder = pickle.load(f)
@@ -42,4 +51,6 @@ model = tf.keras.Sequential(
     
         L.Flatten(), L.Dense(512,activation='relu'), L.BatchNormalization(), L.Dense(7,activation='softmax')
     ]
-); model.load_weights('./model/helper/weights.h5')
+)
+
+model.load_weights('./model/helper/weights.h5')
